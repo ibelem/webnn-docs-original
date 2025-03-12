d2724beec9cc1a4334ac682941a3cb66ca241517
Belem Zhang <belem.zhang@intel.com>
2025-03-12 15:39:53 +0800

    icon

--- middleware.js
+++ middleware.js
@@ -3,6 +3,6 @@ export { middleware } from 'nextra/locales'
 export const config = {
   // Matcher ignoring `/_next/` and `/api/`
   matcher: [
-    '/((?!api|_next/static|_next/image|android-chrome-192x192.png|android-chrome-512x512|apple-touch-icon.png|favicon.ico|icon.svg|apple-icon.png|manifest|_pagefind).*)'
+    '/((?!api|_next/static|_next/image|favicon.ico|icon.svg|apple-icon.png|manifest|_pagefind).*)'
   ]
 }
