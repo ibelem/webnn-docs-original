ec5587f06bc5eabe5769d29eed431172b0ded1b7
Belem Zhang <belem.zhang@intel.com>
2025-05-09 12:55:08 +0800

    GoogleAnalytics for showcase and playground

--- app/[lang]/playground/page.jsx
+++ app/[lang]/playground/page.jsx
@@ -1,7 +1,11 @@
 import { Playground } from '../../_components/playground'
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 export default function Page() {
   return (
-    <Playground isEditorPage={false} />
+    <div>
+      <Playground isEditorPage={false} />
+      <GoogleAnalytics gaId="G-0MBRQQNHVC" />
+    </div>
   );
 }
